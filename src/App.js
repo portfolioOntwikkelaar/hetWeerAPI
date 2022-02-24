ab314e4721645d1e752550dd5f7d320e69ac9c5c
noordseoostende <voropayev1@mail.ru>
2022-02-24 18:32:54 +0100

    het functionaliteit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
+
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [query,setQuery]= useState('');
+  const [cond,setCond]= useState([])
+  const url = `https://api.weatherstack.com/current?access_key=3d6c17c7d958719c3c26db7d5b630224&query=${query}`
+
+  async function getData() {
+    const result = await axios.get(url)
+    setCond([result.data])
+    console.log(result.data)
+  }
+
+  const submitForm = e => {
+    e.preventDefault()
+    getData()
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={submitForm} >
+        <input type="text" placeholder='Where are you?' value={query} onChange={e => setQuery()} />
+        <button></button>
+      </form>
+      {cond.map((item) =>{
+        return (
+          <div>
+            <div></div>
+          </div>
+        )
+      })}
     </div>
   );
 }
